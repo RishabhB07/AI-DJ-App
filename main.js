939c79c19fcf74ca3e21de5a347abab9dff1d006
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-10-08 22:13:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-song1="";
-song2="";
+song="";
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -16,8 +15,12 @@ function draw(){
     image(video, 0, 0, 600, 500);
 }
 function preload(){
-    song=loadSound("Pleasant Music.mp3");
-    song=loadSound("Rock Music.mp3");
+    song=loadSound("music.mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 function modelLoaded(){
     console.log('poseNet is intialized!');
@@ -27,9 +30,9 @@ function gotPoses(results){
         console.log(results);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
-        console.log("leftWristX="+leftWristX+" leftWristY="+leftWristY);
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
         rightWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
-        console.log("rightWristX="+rightWristX+" rightWristY="+rightWristY);
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
     }
 }
